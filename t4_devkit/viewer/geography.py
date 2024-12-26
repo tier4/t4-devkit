a6a72f7690c6699b6812c8108bb3fe7d6811bf3b
Kotaro Uetake <60615504+ktro2828@users.noreply.github.com>
2024-12-26 19:48:16 +0900

    feat: add support of loading global map origin from yaml (#70)
    Signed-off-by: ktro2828 <kotaro.uetake@tier4.jp>

---
+++ t4_devkit/viewer/geography.py
@@ -0,0 +1,49 @@
+from __future__ import annotations
+
+import math
+from typing import TYPE_CHECKING
+
+import numpy as np
+
+if TYPE_CHECKING:
+    from t4_devkit.typing import TranslationType
+
+
+__all__ = ["calculate_geodetic_point"]
+
+EARTH_RADIUS_METERS = 6.378137e6
+FLATTENING = 1 / 298.257223563
+
+
+def calculate_geodetic_point(
+    position: TranslationType,
+    origin: tuple[float, float],
+) -> tuple[float, float]:
+    """Transform a position in a map coordinate system to a position in a geodetic coordinate system.
+
+    Args:
+        position (TranslationType): 3D position in a map coordinate system.
+        origin (tuple[float, float]): Map origin position in a geodetic coordinate system,
+            which is (latitude, longitude).
+
+    Returns:
+        tuple[float, float]: Transformed position in a geodetic coordinate system,
+            which is (latitude, longitude).
+    """
+    x, y, _ = position
+    bearing = math.atan2(x, y)
+    distance = math.hypot(x, y)
+
+    latitude, longitude = np.radians(origin)
+    angular_distance = distance / EARTH_RADIUS_METERS
+
+    target_latitude = math.asin(
+        math.sin(latitude) * math.cos(angular_distance)
+        + math.cos(latitude) * math.sin(angular_distance) * math.cos(bearing)
+    )
+    target_longitude = longitude + math.atan2(
+        math.sin(bearing) * math.sin(angular_distance) * math.cos(latitude),
+        math.cos(angular_distance) - math.sin(latitude) * math.sin(target_latitude),
+    )
+
+    return math.degrees(target_latitude), math.degrees(target_longitude)
