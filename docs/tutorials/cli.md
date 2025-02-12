c127df4bf4a9dd5243cb6c4d010f7e07135f1cc5
Kotaro Uetake <60615504+ktro2828@users.noreply.github.com>
2025-02-13 00:50:34 +0900

    feat: add CLI tool for visualization (#91)
    * feat: add CLI for visualization
    
    Signed-off-by: ktro2828 <kotaro.uetake@tier4.jp>
    
    * docs: add document for CLI
    
    Signed-off-by: ktro2828 <kotaro.uetake@tier4.jp>
    
    ---------
    
    Signed-off-by: ktro2828 <kotaro.uetake@tier4.jp>

---
+++ docs/tutorials/cli.md
@@ -0,0 +1,68 @@
+## Command Line Tools
+
+### CLI Support
+
+Following command line tools are supported:
+
+- `t4viz`: Visualize T4 dataset features.
+
+To enable shell completion, please run following commands:
+
+### Shell Completion
+
+#### Bash
+
+```shell
+_T4VIZ_COMPLETE=bash_source t4viz > $HOME/.t4viz-complete.bash
+
+echo "source $HOME/.t4viz-complete.bash" >> ~/.bashrc
+```
+
+#### Fish
+
+```shell
+_T4VIZ_COMPLETE=fish_source t4viz > $HOME/.config/fish/completions/t4viz.fish
+```
+
+###
+
+### `t4viz`
+
+#### Visualize a Scene
+
+```shell
+t4viz scene <DATA_ROOT> [OPTIONS]
+```
+
+Arguments
+
+- `<DATA_ROOT>`: Root directory path to T4 dataset.
+- `-f, --future <SECONDS>`: Future time seconds.
+- `-o, --output <OUTPUT_DIR>`: Directory path to save recording `.rrd` file.
+- `--no-show`: Indicates whether not to show viewer.
+
+#### Visualize a Specific Instance
+
+```shell
+t4viz instance <DATA_ROOT> <INSTANCE_TOKEN> [OPTIONS]
+```
+
+Arguments
+
+- `<DATA_ROOT>`: Root directory path to T4 dataset.
+- `<INSTANCE_TOKEN>`: Unique identifier token of an instance record.
+- `-f, --future <SECONDS>`: Future time seconds.
+- `-o, --output <OUTPUT_DIR>`: Directory path to save recording `.rrd` file.
+- `--no-show`: Indicates whether not to show viewer.
+
+#### Visualize PointCloud
+
+```shell
+t4viz pointcloud <DATA_ROOT> [OPTIONS]
+```
+
+- `<DATA_ROOT>`: Root directory path to T4 dataset.
+- `<INSTANCE_TOKEN>`: Unique identifier token of an instance record.
+- `-d, --distortion`: Indicates whether not to ignore camera distortion.
+- `-o, --output <OUTPUT_DIR>`: Directory path to save recording `.rrd` file.
+- `--no-show`: Indicates whether not to show viewer.
---
+++ t4_devkit/cli/visualize.py
@@ -0,0 +1,94 @@
+from __future__ import annotations
+
+import os
+
+import click
+
+from t4_devkit import Tier4
+
+
+@click.group(invoke_without_command=True)
+@click.pass_context
+def main(ctx: click.Context) -> None:
+    if ctx.invoked_subcommand is None:
+        print(ctx.get_help())
+    else:
+        ctx.invoked_subcommand
+
+
+@main.command(name="scene", help="Visualize a specific scene.")
+@click.argument("data_root", type=click.Path(exists=True))
+@click.option("-f", "--future", type=float, default=0.0, help="Future time seconds.")
+@click.option(
+    "-o",
+    "--output",
+    type=click.Path(writable=True),
+    help="Output directory to save recoding .rrd file.",
+)
+@click.option("--no-show", is_flag=True, help="Indicates whether not to show viewer.")
+def scene(data_root: str, output: str | None, future: float, no_show: bool) -> None:
+    _create_dir(output)
+
+    t4 = Tier4("annotation", data_root, verbose=False)
+    scene_token = t4.scene[0].token
+    t4.render_scene(scene_token, future_seconds=future, save_dir=output, show=not no_show)
+
+
+@main.command(name="instance", help="Visualize a particular instance in a corresponding scene")
+@click.argument("data_root", type=click.Path(exists=True))
+@click.argument("instance", type=click.STRING)
+@click.option("-f", "--future", type=float, default=0.0, help="Future time seconds.")
+@click.option(
+    "-o",
+    "--output",
+    type=click.Path(writable=True),
+    help="Output directory to save recoding .rrd file.",
+)
+@click.option("--no-show", is_flag=True, help="Indicates whether not to show viewer.")
+def instance(
+    data_root: str, instance: str, future: float, output: str | None, no_show: bool
+) -> None:
+    _create_dir(output)
+
+    t4 = Tier4("annotation", data_root, verbose=False)
+    t4.render_instance(
+        instance_token=instance, future_seconds=future, save_dir=output, show=not no_show
+    )
+
+
+@main.command(name="pointcloud", help="Visualize pointcloud in a corresponding scene.")
+@click.argument("data_root", type=click.Path(exists=True))
+@click.option(
+    "-d",
+    "--distortion",
+    is_flag=True,
+    help="Indicates whether not to ignore camera distortion.",
+)
+@click.option(
+    "-o",
+    "--output",
+    type=click.Path(writable=True),
+    help="Output directory to save recoding .rrd file.",
+)
+@click.option("--no-show", is_flag=True, help="Indicates whether not to show viewer.")
+def pointcloud(data_root: str, distortion: bool, output: str | None, no_show: bool) -> None:
+    _create_dir(output)
+
+    t4 = Tier4("annotation", data_root, verbose=False)
+    scene_token = t4.scene[0].token
+    t4.render_pointcloud(
+        scene_token,
+        ignore_distortion=not distortion,
+        save_dir=output,
+        show=not no_show,
+    )
+
+
+def _create_dir(dir_path: str) -> None:
+    """Create a directory with the specified path.
+    If the input path is `None` this function does nothing.
+    Args:
+        dir_path (str | None): Directory path to create.
+    """
+    if dir_path is not None:
+        os.makedirs(dir_path, exist_ok=True)
