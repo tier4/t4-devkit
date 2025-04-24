fb1f511cb881420fa890b5d313e2aed5336ab715
ktro2828 <kotaro.uetake@tier4.jp>
2025-04-25 03:03:14 +0900

    refactor: make serialize_schema(...) as deprecated
    Signed-off-by: ktro2828 <kotaro.uetake@tier4.jp>

--- t4_devkit/schema/serialize.py
+++ t4_devkit/schema/serialize.py
@@ -2,7 +2,9 @@ from __future__ import annotations
 
 from typing import TYPE_CHECKING
 
-from t4_devkit.common.serialize import serialize_dataclass
+from typing_extensions import deprecated
+
+from t4_devkit.common.serialize import serialize_dataclass, serialize_dataclasses
 
 if TYPE_CHECKING:
     from .tables import SchemaTable
@@ -11,8 +13,12 @@ if TYPE_CHECKING:
 __all__ = ("serialize_schemas", "serialize_schema")
 
 
+@deprecated("`serialize_schemas()` is deprecated. Please use `serialize_dataclasses()` instead.")
 def serialize_schemas(data: list[SchemaTable]) -> list[dict]:
     """Serialize a list of schema dataclasses into list of dict.
+
+    Deprecated:
+        This function is deprecated. Please use `serialize_dataclasses()` instead.
 
     Args:
         data (list[SchemaTable]): List of schema dataclasses.
@@ -20,11 +26,15 @@ def serialize_schemas(data: list[SchemaTable]) -> list[dict]:
     Returns:
         Serialized list of dict data.
     """
-    return [serialize_schema(d) for d in data]
+    return serialize_dataclasses(data)
 
 
+@deprecated("`serialize_schema()` is deprecated. Please use `serialize_dataclass()` instead.")
 def serialize_schema(data: SchemaTable) -> dict:
     """Serialize a schema dataclass into dict.
+
+    Deprecated:
+        This function is deprecated. Please use `serialize_dataclass()` instead.
 
     Args:
         data (SchemaTable): Schema dataclass.
