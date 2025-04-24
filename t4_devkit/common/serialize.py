5ac2eca0d5372f7e71e63c0f7f1c990e55b64e06
ktro2828 <kotaro.uetake@tier4.jp>
2025-04-25 02:35:30 +0900

    feat: add serializer for list of dataclasses
    Signed-off-by: ktro2828 <kotaro.uetake@tier4.jp>

--- t4_devkit/common/serialize.py
+++ t4_devkit/common/serialize.py
@@ -8,6 +8,18 @@ from attrs import asdict, fields, filters
 from pyquaternion import Quaternion
 
 
+def serialize_dataclasses(data: list[Any]) -> list[dict]:
+    """Serialize list of attrs' dataclasses into list of dict.
+
+    Args:
+        data (list[Any]): List of attrs' dataclasses.
+
+    Returns:
+        Serialized list of dict data.
+    """
+    return [serialize_dataclass(d) for d in data]
+
+
 def serialize_dataclass(data: Any) -> dict[str, Any]:
     """Serialize attrs' dataclasses into dict.
 
@@ -17,7 +29,7 @@ def serialize_dataclass(data: Any) -> dict[str, Any]:
         data (Any): Dataclass object.
 
     Returns:
-        dict[str, Any]: Serialized dict.
+        Serialized dict.
     """
     excludes = filters.exclude(*[a for a in fields(data.__class__) if not a.init])
     return asdict(data, filter=excludes, value_serializer=_value_serializer)
