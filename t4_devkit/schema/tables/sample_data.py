f29ec94a6a30e5b34fe82aa3245f2428e7d1bb91
Samrat Thapa <samratthapa120@gmail.com>
2025-08-28 11:05:32 +0900

    added optional string

--- t4_devkit/schema/tables/sample_data.py
+++ t4_devkit/schema/tables/sample_data.py
@@ -1,7 +1,7 @@
 from __future__ import annotations
 
 from enum import Enum, unique
-from typing import TYPE_CHECKING
+from typing import TYPE_CHECKING, Optional
 
 from attrs import define, field, validators
 
@@ -104,7 +104,7 @@ class SampleData(SchemaBase):
     next: str = field(validator=validators.instance_of(str))  # noqa: A003
     prev: str = field(validator=validators.instance_of(str))
     is_valid: bool = field(default=True, validator=validators.instance_of(bool))
-    info_filename: str | None = field(default=None, validator=validators.instance_of(str))
+    info_filename: str | None = field(default=None, validator=validators.instance_of(Optional[str]))
 
     # shortcuts
     modality: SensorModality | None = field(init=False, default=None)
