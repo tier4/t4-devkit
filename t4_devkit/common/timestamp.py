3038e57c043214b66e711fdff96ac7ec786f0d90
ktro2828 <kotaro.uetake@tier4.jp>
2025-11-18 22:14:20 +0900

    refactor: replace sec2us and us2sec to seconds2microseconds and microseconds2seconds
    Signed-off-by: ktro2828 <kotaro.uetake@tier4.jp>

--- t4_devkit/common/timestamp.py
+++ t4_devkit/common/timestamp.py
@@ -2,13 +2,45 @@ from __future__ import annotations
 
 from typing import TYPE_CHECKING
 
+from typing_extensions import deprecated
+
 if TYPE_CHECKING:
     from t4_devkit.typing import ScalarLike
 
-__all__ = ("us2sec", "sec2us")
 
-
+@deprecated("Please use `microseconds2seconds(...)` instead.")
 def us2sec(timestamp: ScalarLike) -> float:
+    """Convert timestamp from micro seconds [us] to seconds [s].
+
+    Deprecated:
+        This function is depracated. Please use `microseconds2seconds(...)` instead.
+
+    Args:
+        timestamp (ScalarLike): Timestamp in [us].
+
+    Returns:
+        Timestamp in [s].
+    """
+    return microseconds2seconds(timestamp)
+
+
+@deprecated("Please use `seconds2microseconds(...)` instead.")
+def sec2us(timestamp: ScalarLike) -> float:
+    """Convert timestamp from seconds [s] to micro seconds [us].
+
+    Deprecated:
+        This function is depracated. Please use `seconds2microseconds(...)` instead.
+
+    Args:
+        timestamp (ScalarLike): Timestamp in [s].
+
+    Returns:
+        Timestamp in [us].
+    """
+    return seconds2microseconds(timestamp)
+
+
+def microseconds2seconds(timestamp: ScalarLike) -> float:
     """Convert timestamp from micro seconds [us] to seconds [s].
 
     Args:
@@ -20,7 +52,7 @@ def us2sec(timestamp: ScalarLike) -> float:
     return 1e-6 * timestamp
 
 
-def sec2us(timestamp: ScalarLike) -> float:
+def seconds2microseconds(timestamp: ScalarLike) -> float:
     """Convert timestamp from seconds [s] to micro seconds [us].
 
     Args:
