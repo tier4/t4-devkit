2189ddefae515ad3615f1fbcd99b5556368210f1
Kotaro Uetake <60615504+ktro2828@users.noreply.github.com>
2026-01-30 01:33:00 +0900

    feat(evaluation): add evaluator (#154)
    * feat: add evaluator
    
    Signed-off-by: ktro2828 <kotaro.uetake@tier4.jp>
    
    * fix: update the usage of Tier4
    
    Signed-off-by: ktro2828 <kotaro.uetake@tier4.jp>
    
    ---------
    
    Signed-off-by: ktro2828 <kotaro.uetake@tier4.jp>

--- t4_devkit/evaluation/task.py
+++ t4_devkit/evaluation/task.py
@@ -1,25 +1,51 @@
 from __future__ import annotations
 
-from enum import Enum
+from enum import Enum, unique
 
 __all__ = ["EvaluationTask"]
 
 
+@unique
 class EvaluationTask(str, Enum):
     """Enumeration of evaluation tasks."""
 
     DETECTION3D = "detection3d"
     TRACKING3D = "tracking3d"
     PREDICTION3D = "prediction3d"
+    SEGMENTATION3D = "segmentation3d"
     DETECTION2D = "detection2d"
     TRACKING2D = "tracking2d"
+    SEGMENTATION2D = "segmentation2d"
 
     def is_3d(self) -> bool:
+        """Indicate whether the task considers 3D objects.
+
+        Returns:
+            Return `True` if the task is in [detection3d, tracking3d, prediction3d, segmentation3d].
+        """
         return self in (
             EvaluationTask.DETECTION3D,
             EvaluationTask.TRACKING3D,
             EvaluationTask.PREDICTION3D,
+            EvaluationTask.SEGMENTATION3D,
         )
 
     def is_2d(self) -> bool:
-        return self in (EvaluationTask.DETECTION2D, EvaluationTask.TRACKING2D)
+        """Indicate whether the task considers 2D objects.
+
+        Returns:
+            Return `True` if the task is in [detection2d, tracking2d, segmentation2d].
+        """
+        return self in (
+            EvaluationTask.DETECTION2D,
+            EvaluationTask.TRACKING2D,
+            EvaluationTask.SEGMENTATION2D,
+        )
+
+    def is_segmentation(self) -> bool:
+        """Indicate whether the task deals with segmentation.
+
+        Returns:
+            Return `True` if the task is in [segmentation3d, segmentation2d].
+        """
+        return self in (EvaluationTask.SEGMENTATION3D, EvaluationTask.SEGMENTATION2D)
