ff184f8998657bbd316257eff826aa712daf7c71
ktro2828 <kotaro.uetake@tier4.jp>
2026-04-03 16:34:59 +0900

    feat: update score to optional
    Signed-off-by: ktro2828 <kotaro.uetake@tier4.jp>

--- t4_devkit/schema/tables/autolabel_metadata.py
+++ t4_devkit/schema/tables/autolabel_metadata.py
@@ -11,14 +11,17 @@ class AutolabelModel:
 
     Attributes:
         name (str): Name of the model used for annotation. Can include version information.
-        score (float): Label score for the annotation from this model (range: 0.0–1.0).
+        score (float | None, optional): Label score for the annotation from this model (range: 0.0–1.0).
         uncertainty (float | None, optional): Model-reported uncertainty for the annotation (range: 0.0–1.0).
             Lower values imply higher confidence.
     """
 
     name: str = field(validator=validators.instance_of(str))
-    score: float = field(
-        validator=(validators.instance_of(float), validators.ge(0.0), validators.le(1.0))
+    score: float | None = field(
+        default=None,
+        validator=validators.optional(
+            validators.and_(validators.instance_of(float), validators.ge(0.0), validators.le(1.0))
+        ),
     )
     uncertainty: float | None = field(
         default=None,
